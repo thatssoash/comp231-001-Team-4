13610350c0e6d5ac4345cd1dca5ab580898e3c16
ArshdeepSingh56 <67112828+ArshdeepSingh56@users.noreply.github.com>
2021-03-24 01:56:05 +0530

    Business Type updated
    Business Type updated

--- AccountApp.BOL/Business.cs
+++ AccountApp.BOL/Business.cs
@@ -1,6 +1,8 @@
 ﻿#region Header
 
 #endregion
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 /// <summary>
 /// Model class for Business
 /// </summary>
@@ -8,6 +10,17 @@ namespace AccountApp.BOL
 {
     public class BusinessModel
     {
+        [Key]
         public int Id { get; set; }
+
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string BusinessNo { get; set; }
+
+        public string BusinessName { get; set; }
+        public string HstGstNumber { get; set; }
+        public PhoneAttribute Phone { get; set; }
+        public EmailAddressAttribute Email { get; set; }
+        public string Address { get; set; }
+
     }
 }

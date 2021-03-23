b5fb8829ab9f7b251757cbc55acac32278858b70
ArshdeepSingh56 <67112828+ArshdeepSingh56@users.noreply.github.com>
2021-03-23 17:46:16 +0530

    Business type added in model
    Business type added in model

---
+++ AccountApp.BOL/Business.cs
@@ -0,0 +1,13 @@
+﻿#region Header
+
+#endregion
+/// <summary>
+/// Model class for Business
+/// </summary>
+namespace AccountApp.BOL
+{
+    public class BusinessModel
+    {
+        public int Id { get; set; }
+    }
+}

c1b4815cc5494c258e6d0de1fa25512ad0235d03
jagjitsingh-code <67120635+jagjitsingh-code@users.noreply.github.com>
2021-03-24 03:43:58 +0530

    New Business Form on UI till DB
    New Business Form on UI till DB

--- AccountApp.BOL/Business.cs
+++ AccountApp.BOL/Business.cs
@@ -9,11 +9,25 @@ namespace AccountApp.BOL
     {
         [Key]
         public int Id { get; set; }
+
+        [Required]
+        [Display(Name ="Business No.")]
         public string BusinessNo { get; set; }
 
+        [Required]
+        [Display(Name = "Business Name")]
         public string BusinessName { get; set; }
+
+        [Required]
+        [Display(Name = "Hst Or Gst Number")]
         public string HstGstNumber { get; set; }
+
+        [Display(Name = "Phone Number")]
+        [Phone]
         public string Phone { get; set; }
+
+        [Display(Name = "Email")]
+        [EmailAddress]
         public string Email { get; set; }
         public string Address { get; set; }
     }
---
+++ AccountApp.BOL/Migrations/20210323214050_BusinessTypeValidation.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AccountApp.BOL.Migrations
+{
+    public partial class BusinessTypeValidation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "HstGstNumber",
+                table: "Businesses",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BusinessNo",
+                table: "Businesses",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BusinessName",
+                table: "Businesses",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "HstGstNumber",
+                table: "Businesses",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BusinessNo",
+                table: "Businesses",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BusinessName",
+                table: "Businesses",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}

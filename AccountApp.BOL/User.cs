0eb7f3bb0cc02d234f2c59d0f01e1f75c21037ea
aditi0809 <68449254+aditi0809@users.noreply.github.com>
2021-03-25 20:40:29 +0530

    User type created
    User type created

---
+++ AccountApp.BOL/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AccountApp.BOL
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+    }
+}

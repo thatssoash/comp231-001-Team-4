5f4acc1f4225ef5c0dffd2f2f54c663fa0155bd8
ArshdeepSingh56 <67112828+ArshdeepSingh56@users.noreply.github.com>
2021-03-26 01:29:38 +0530

    Connect Database to UI (financial documents) FORMS
    Connect Database to UI (financial documents) FORMS

---
+++ AccountApp.BOL/CustomForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace AccountApp.BOL
+{
+    public class CustomForm
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Number { get; set; }
+
+        [Display(Name ="Form Name")]
+        public string FormName { get; set; }
+        public string Code { get; set; }
+        public string Title { get; set; }
+        public string Status { get; set; }
+    }
+}
---
+++ AccountApp.BOL/Repository/FormDBL.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AccountApp.BOL.Repository
+{
+    public interface IFormDBL
+    {
+        IEnumerable<CustomForm> GetCustomForms();
+        IEnumerable<CustomForm> GetCustomForms(string name);
+        CustomForm GetCustomForm(int id);
+        bool Delete(int id);
+        bool Save(CustomForm form);
+        bool Update(CustomForm form);
+    }
+
+    public class FormDBL : IFormDBL
+    {
+        private AccountAppDbContext _db;
+        public FormDBL(AccountAppDbContext db)
+        {
+            this._db = db;
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CustomForm GetCustomForm(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<CustomForm> GetCustomForms()
+        {
+            return _db.CustomForms;
+        }
+
+        public IEnumerable<CustomForm> GetCustomForms(string name)
+        {
+            return _db.CustomForms.Where(x=>x.FormName.Contains(name));
+        }
+
+        public bool Save(CustomForm form)
+        {
+            try
+            {
+                _db.CustomForms.Add(form);
+                return true;
+            }
+            catch(Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public bool Update(CustomForm form)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

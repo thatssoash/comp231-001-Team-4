2c6a7975d6497e623d8fb0e8a90e12c17ebf6fe0
Maziar Hassanzadeh <maziar.hassanzadeh@gmail.com>
2021-04-04 21:27:47 -0400

    User Model

---
+++ AccountApp/Models/AppUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AccountApp.Models
+{
+    public class AppUser
+    {
+        public string SIN { get; set; }
+
+        public string Address { get; set; }
+
+        public int BusinessCount { get; set; }
+
+        public DateTime DateEdit { get; set; } = DateTime.Now;
+    }
+}

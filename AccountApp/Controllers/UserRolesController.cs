987aa2ed5c11ffb8c2a4c59e7ee358739e1a3877
ArshdeepSingh56 <67112828+ArshdeepSingh56@users.noreply.github.com>
2021-04-06 20:19:02 +0530

    CRUD controller to create/edit user roles
    CRUD controller to create/edit user roles

---
+++ AccountApp/Controllers/UserRolesController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AccountApp.BOL;
+
+namespace AccountApp.Controllers
+{
+    public class UserRolesController : Controller
+    {
+        private readonly AccountAppDbContext _context;
+
+        public UserRolesController(AccountAppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: UserRoles
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.UserRoles.ToListAsync());
+        }
+
+        // GET: UserRoles/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var userRoles = await _context.UserRoles
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (userRoles == null)
+            {
+                return NotFound();
+            }
+
+            return View(userRoles);
+        }
+
+        // GET: UserRoles/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UserRoles/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Role,RoleName")] UserRoles userRoles)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(userRoles);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(userRoles);
+        }
+
+        // GET: UserRoles/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var userRoles = await _context.UserRoles.FindAsync(id);
+            if (userRoles == null)
+            {
+                return NotFound();
+            }
+            return View(userRoles);
+        }
+
+        // POST: UserRoles/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Role,RoleName")] UserRoles userRoles)
+        {
+            if (id != userRoles.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(userRoles);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UserRolesExists(userRoles.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(userRoles);
+        }
+
+        // GET: UserRoles/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var userRoles = await _context.UserRoles
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (userRoles == null)
+            {
+                return NotFound();
+            }
+
+            return View(userRoles);
+        }
+
+        // POST: UserRoles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var userRoles = await _context.UserRoles.FindAsync(id);
+            _context.UserRoles.Remove(userRoles);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UserRolesExists(int id)
+        {
+            return _context.UserRoles.Any(e => e.Id == id);
+        }
+    }
+}
